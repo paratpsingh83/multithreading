40ea37acefa9e8d4375c5e7e556688e83da309e1
Pratap Singh <pratapsingh839399@gmail.com>
2025-12-11 15:43:46 +0530

    10 commit

---
+++ src/com/globalsoft/mg/enums/PaymentStatus.java
@@ -0,0 +1,7 @@
+package com.jobportal.enums;
+
+public enum PaymentStatus {
+        PENDING,
+        COMPLETED,
+        FAILED
+    }
---
+++ src/com/globalsoft/mg/enums/ProficiencyLevel.java
@@ -0,0 +1,5 @@
+package com.jobportal.enums;
+
+public enum ProficiencyLevel {
+    BEGINNER, INTERMEDIATE, EXPERT
+}
