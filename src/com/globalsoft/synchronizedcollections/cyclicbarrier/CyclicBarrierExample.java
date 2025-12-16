3a9d2df3545a45e473ab8be3c0c2f2aa0589f6e2
Pratap Singh <pratapsingh839399@gmail.com>
2025-12-16 10:26:50 +0530

    Cyclic Barrier example commit

--- src/com/globalsoft/synchronizedcollections/cyclicbarrier/CyclicBarrierExample.java
+++ src/com/globalsoft/synchronizedcollections/cyclicbarrier/CyclicBarrierExample.java
@@ -1,5 +1,48 @@
 package com.globalsoft.synchronizedcollections.cyclicbarrier;
 
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class BarrierWorker implements Runnable {
+    private int id;
+    private Random random;
+    private CyclicBarrier cyclicBarrier;
+
+    public BarrierWorker(int id, CyclicBarrier cyclicBarrier) {
+        this.id = id;
+        this.random = new Random();
+        this.cyclicBarrier = cyclicBarrier;
+    }
+
+    @Override
+    public void run() {
+        doWork();
+    }
+
+    private void doWork() {
+
+        System.out.println("Thread with id " + this.id + " start the work...");
+        try {
+            Thread.sleep(random.nextInt(3000));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            //when all the thread call await() this is when
+            //the "barrier is broken"
+            cyclicBarrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("After the await()...");
+    }
+
+}
+
 public class CyclicBarrierExample {
     /*
      * Latch -> a single thread can wait for other threads
@@ -19,6 +62,20 @@ public class CyclicBarrierExample {
      */
 
     public static void main(String[] args) {
+
+        ExecutorService service = Executors.newFixedThreadPool(5);
+
+        //5 is the number of parties(the number of Threads)
+        CyclicBarrier barrier = new CyclicBarrier(5, new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("All Task have been finished...");
+            }
+        });
+        for (int i = 0; i < 5; i++) {
+            service.execute(new BarrierWorker(i + 1, barrier));
+        }
+        service.shutdown();
 
     }
 
